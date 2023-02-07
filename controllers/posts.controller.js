05ba9d13264ceba61e1f005d0150af33be28887d
shahariartushar <shahariartushar@gmail.com>
2023-02-07 17:36:51 +0100

    feat: create like and unlike in a post

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -81,7 +81,78 @@ export const deletePostController = async (req, res) => {
   } catch (err) {
     console.error(err.message);
 
-    if (err.kind == 'ObjectId') {
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+};
+
+export const addPostLikeController = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Check post
+    if (!post) {
+      return res.status(400).json({ msg: 'Post not found' });
+    }
+
+    // Check the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+};
+
+export const addPostUnlikeController = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Check post
+    if (!post) {
+      return res.status(400).json({ msg: 'Post not found' });
+    }
+
+    // Check the post hasn't yet been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
       return res.status(400).json({ msg: 'Post not found' });
     }
 
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,8 @@ import {
   getAllPostsController,
   getPostByIdController,
   deletePostController,
+  addPostLikeController,
+  addPostUnlikeController,
 } from '../../controllers/posts.controller.js';
 
 const router = express.Router();
@@ -29,5 +31,15 @@ router.get('/:post_id', authentication, getPostByIdController);
 // @desc    Delete a post
 // @access  Private
 router.delete('/:post_id', authentication, deletePostController);
+
+// @route   PUT api/posts/like/:post_id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:post_id', authentication, addPostLikeController);
+
+// @route   PUT api/posts/unlike/:post_id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:post_id', authentication, addPostUnlikeController);
 
 export default router;
