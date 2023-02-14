b3cd3b79412dca3810f430c6776d2cf7e00e3e49
shahariartushar <shahariartushar@gmail.com>
2023-02-14 03:26:16 +0100

    feat: add register, login page with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,45 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing.js';
 import Login from './components/auth/Login.js';
 import Register from './components/auth/Register.js';
+import store from './store.js';
+import Alert from './components/layout/Alert.js';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={<Landing />}
-          />
-        </Routes>
-        <section className='container'>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Routes>
             <Route
               exact
-              path='/register'
-              element={<Register />}
-            />
-            <Route
-              exact
-              path='/login'
-              element={<Login />}
+              path='/'
+              element={<Landing />}
             />
           </Routes>
-        </section>
-      </Fragment>
-    </Router>
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route
+                exact
+                path='/register'
+                element={<Register />}
+              />
+              <Route
+                exact
+                path='/login'
+                element={<Login />}
+              />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import alert from './alert.js';
+
+export default combineReducers({ alert });
